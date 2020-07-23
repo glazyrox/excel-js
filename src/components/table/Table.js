18a4a6f3e805c8abc5e629b1e0418767d24b5af7
glazyrox <glazyrox@yandex.ru>
2020-07-24 00:33:53 +0300

    column resize - done, row resize - in progress

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,12 +1,14 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
 import { createTable } from "./table.template";
 
+import { $ } from '../../core/dom';
+
 export class Table extends ExcelComponent {
     static className = 'excel__table';
 
     constructor($root) {
         super($root, {
-            listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+            listeners: ['mousedown']
         })
     }
 
@@ -14,19 +16,42 @@ export class Table extends ExcelComponent {
         return createTable(26);
     }
 
-    onClick() {
-        console.log('click!');
-    }
-
     onMousedown(event) {
-        console.log('mousedown!', event.target);
-    }
+        if (event.target.dataset.resize) {
+            let pageAxis = 'pageX';
+            let dataAttribute = 'col'
+            let moveSide = 'right';
+            let spotMove = 'width';
 
-    onMousemove() {
-        console.log('mousemov');
-    }
 
-    onMouseup() {
-        console.log('mouseup');
+            const $resizer = $(event.target);
+            // const $parent = $resizer.$el.parentNode; // bad cus js-logic vs view-logic
+            // const $parent = $resizer.$el.closest('.column'); too bad cus js-logic vs view-logic
+            const $parent = $resizer.closest('[data-type="resizable"]'); // ближайший родитель по дата-атрибуту
+            const coords = $parent.getCoords(); // коорды элемента
+            const colAtribute = $parent.data.col;
+            // const editableElems = Array.from(document.querySelectorAll(`[data-${attribute}="${colAtribute}"]`)); // query to doc is slow
+
+            if ($parent.data.elem) {
+                pageAxis = 'pageY';
+                dataAttribute = 'row'
+                moveSide = 'bottom';
+                spotMove = 'height';
+            }
+
+            const editableElems = Array.from(this.$root.findAll(`[data-${dataAttribute}="${colAtribute}"]`));
+            let value;
+
+            document.onmousemove = e => {
+                const delta = e[pageAxis] - coords[moveSide];
+                value = (coords[spotMove] + delta);
+            }
+
+            document.onmouseup = () => {
+                console.log(editableElems);
+                editableElems.map(item => item.style[spotMove] = value + 'px');
+                document.onmousemove = null; // delete mousemove for speed
+            }
+        }
     }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,33 +3,32 @@ const ASCI_CODES = {
     Z: 90
 };
 
-function toCell() {
-
+function toCell(_, index) {
     return `
-        <div class="cell" contenteditable=""></div>
+        <div class="cell" contenteditable="" data-col="${index + 1}" data-row="${index + 1}">
+        </div>
     `
 }
 
-function toColumn(content) {
-
+function toColumn(content, index) {
     return `
-        <div class="column">
+        <div class="column" data-type="resizable" data-col="${index + 1}">
             ${content}
-            <div class="col-resize"></div>
+            <div class="col-resize" data-resize="col" ></div>
         </div>
     `
 }
 
 function createRow(content, number = '') {
-    const resizer = number ? `<div class="row-resize"></div>` : '';
+    const resizer = number ? '<div class="row-resize" data-resize="row" ></div>' : '';
     
     return `
         <div class="row">
-            <div class="row__info">
+            <div class="row__info" data-type="resizable" data-elem="row" data-row="${number}">
                 ${number}
                 ${resizer}
             </div>
-            <div class="row__data">${content}</div>
+            <div class="row__data" data-type="row">${content}</div>
         </div>
     `
 }
@@ -46,7 +45,7 @@ export function createTable(rowsCount = 14) {
     const colsCount = ASCI_CODES.Z - ASCI_CODES.A + 1;
     const rows = [];
 
-    const cols = new Array(colsCount)
+    const cols = new Array(colsCount) // column
         .fill('')
         .map(toChar)
         .map(toColumn)
